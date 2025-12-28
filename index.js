4d10cecb52360886dc04687c6f0942ab9990a4ee
Mohamad moustafa <52231128@students.liu.edu.lb>
2025-12-28 11:23:11 +0200

    I created a database named todolist and connected to it via backend

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 import express from "express";
-import cors from 'cors'
+import cors from 'cors';
+import mysql from "mysql2";
 const app = express();
 
-app.listen(8081, () => {
-  console.log("Here");
+app.use(cors());
+app.use(express.json());
+app.listen(5000, () => {
+  console.log("Server is Running");
+});
+
+const db=mysql.createPool({
+  port:3306,
+  host:"localhost",
+  user:"root",
+  password:"",
+  database:"todolist"
+});
+db.getConnection((err) => {
+  if (err) {
+    console.error("Database connection failed:", err);
+  } else {
+    console.log("Connected to MySQL database");
+  }
 });
