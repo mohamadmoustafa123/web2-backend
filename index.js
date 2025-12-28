803987e9152a83bed55fb93f56ed2e651952f960
Mohamad moustafa <52231128@students.liu.edu.lb>
2025-12-28 12:02:52 +0200

    I created an API to add posts to the database. It has errors of type 400 if the request is incomplete or missing, 500 if it is unexpected, and 201 if the request is correct

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import cors from 'cors';
+import cors from "cors";
 import mysql from "mysql2";
 const app = express();
 
@@ -9,12 +9,12 @@ app.listen(5000, () => {
   console.log("Server is Running");
 });
 
-const db=mysql.createPool({
-  port:3306,
-  host:"localhost",
-  user:"root",
-  password:"",
-  database:"todolist"
+const db = mysql.createPool({
+  port: 3306,
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "todolist",
 });
 db.getConnection((err) => {
   if (err) {
@@ -24,16 +24,33 @@ db.getConnection((err) => {
   }
 });
 
-app.get("/tasks",(req,res)=>{
-const q="SELECT * FROM todolist";
-db.query(q,(err,data)=>{
-if(err){
-  return res.status(500).json({message:"Database error",error:err})
-}else{
-  if(data.length===0){
-    return res.status(204).send("not found")
-  }
-  return res.status(200).json(data)
-}
+app.get("/tasks", (req, res) => {
+  const q = "SELECT * FROM tasks";
+  db.query(q, (err, data) => {
+    if (err) {
+      return res.status(500).json({ message: "Database error", error: err });
+    } else {
+      if (data.length === 0) {
+        return res.status(204).send("not found");
+      }
+      return res.status(200).json(data);
+    }
+  });
 });
+
+app.post("/tasks", (req, res) => {
+  if (!req.body) {
+    return res.status(400).send("Request body is missing");
+  }
+  const { title } = req.body;
+  const q = "INSERT INTO student (tasks) VALUES (?)";
+  db.query(q, [title], (err, data) => {
+    if (err) {
+      return res.status(500).json({message:"Database error",error:err})
+    }else{
+      return res.status(201).json({message:"Tasks created successfully",
+        id:data.insertId
+      })
+    }
+  });
 });
